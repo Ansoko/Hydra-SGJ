923b7c03615b54f4d1f678751aa4714216c23d4e
Marguerite Leang <marguerite.leang@gmail.com>
2024-03-23 11:03:35 +0100

    Add first version of principal menu

---
+++ Assets/Project/Scripts/MenuController.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+    public GameObject Menu_Canvas;
+    public GameObject Contexte_Canvas;
+    public GameObject Regles_Canvas;
+    public GameObject Credits_Canvas;
+
+
+    public void Menu(){
+        Menu_Canvas.SetActive(true);
+        Contexte_Canvas.SetActive(false);
+        Regles_Canvas.SetActive(false);
+        Credits_Canvas.SetActive(false);
+    }
+
+
+    public void Contexte(){
+        Menu_Canvas.SetActive(false);
+        Contexte_Canvas.SetActive(true);
+        Regles_Canvas.SetActive(false);
+        Credits_Canvas.SetActive(false);
+    }
+
+
+    public void Regles(){
+        Menu_Canvas.SetActive(false);
+        Regles_Canvas.SetActive(true);
+        Contexte_Canvas.SetActive(false);
+        Credits_Canvas.SetActive(false);
+
+    }
+
+
+    public void Credits(){
+        Menu_Canvas.SetActive(false);
+        Credits_Canvas.SetActive(true);
+        Contexte_Canvas.SetActive(false);
+        Regles_Canvas.SetActive(false);
+
+    }
+
+
+    public void Jouer(){
+        SceneManager.LoadScene("Principal_Menu");
+    }
+
+}
